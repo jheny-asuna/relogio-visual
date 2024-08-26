9dae05052b9d58c2a66afd7956fc3e2b469203e4
Jheniffer Rodrigues <jheniffersilvarodrigues@gmail.com>
2024-08-26 16:14:11 +0100

    Alteração de mensagem de abertura

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
+    var cumprimentos = window.document.getElementById('bom')
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas`
     if (hora >= 5 && hora < 12) {
-        //BOM DIAA!
+        cumprimentos.innerHTML = `BOM DIAA!`
         img.src = 'manha.png'
         document.body.style.background = '#CAC9C5'
     } else if (hora >= 12 && hora <= 18) {
-        //BOA TARDEE!
+        cumprimentos.innerHTML = `BOA TARDEE!`
         img.src = 'tarde.png'
         document.body.style.background = '#E9B193'
     } else {
-        //BOA NOITEE!
+        cumprimentos.innerHTML = `BOA NOITEE!`
         img.src = 'noite.png'
         document.body.style.background = '#415C61'
     }
