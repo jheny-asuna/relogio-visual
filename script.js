cb31079fbeb1504ff91dbc3a648431b2cac7f7cb
Jheniffer Rodrigues <jheniffersilvarodrigues@gmail.com>
2024-08-26 15:21:48 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 5 && hora < 12) {
+        //BOM DIAA!
+        img.src = 'manha.png'
+        document.body.style.background = '#CAC9C5'
+    } else if (hora >= 12 && hora <= 18) {
+        //BOA TARDEE!
+        img.src = 'tarde.png'
+        document.body.style.background = '#E9B193'
+    } else {
+        //BOA NOITEE!
+        img.src = 'noite.png'
+        document.body.style.background = '#415C61'
+    }
+}
